7c4cdd6585fef4e01319e8800acf971e6209489c
KungKurma <cocosaboko@protonmail.ch>
2021-09-18 14:13:36 +0700

    main

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const mysql = require('mysql')
 // Panggil express function
 const app = express()
 
+function feedbackSuccess() {
+    console.log("Server started...")
+}
+
+// Start Server
+app.listen(3000, feedbackSuccess)
