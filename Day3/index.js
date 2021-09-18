fd46896bb42e21d24a77e3ab78ab10edbce1c043
KungKurma <cocosaboko@protonmail.ch>
2021-09-18 15:26:31 +0700

    main

--- Day3/index.js
+++ Day3/index.js
@@ -1,15 +1,21 @@
 // Library
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
 // Panggil express function
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
 
 // Database connection
 const db = mysql.createConnection({
     host: "localhost", 
     user: "root",
-    password: "8!hUnrBeLKAeEftewsO#c%k^XJ@O0SMW$tm10p4AF^Gb4wS1G93JM6GfzJBlz^^CH!2NpWxvRBfno4GDuKJVKeCTdefp#^k**v4s8wqfGBDpf6yE8tf&HYlbFW!%&H56",
+    password: "rootpass",
     database: "bootcampday3"
 });
 
@@ -25,6 +31,21 @@ app.get("/", (req, res) => {
 
 app.get("/test", (req, res) => {
     res.send("Hello World")
+});
+
+app.get("/readdata", (req, res) => {
+    const sql = "SELECT * FROM matkul";
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result)
+    });
+});
+
+app.post("/postdata/:nama", (req, res) => {
+    console.log(req.body);
+    console.log(req.body.nama);
+    console.log(req.body.kode);
+    res.send("Data sent");
 });
 
 // Start Server
