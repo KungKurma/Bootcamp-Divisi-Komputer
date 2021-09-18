fe4305a6e0c176e29f56fafaef492ed0805aaaee
KungKurma <cocosaboko@protonmail.ch>
2021-09-18 14:33:55 +0700

    main

--- Day3/index.js
+++ Day3/index.js
@@ -5,6 +5,14 @@ const mysql = require('mysql');
 // Panggil express function
 const app = express();
 
+// Database connection
+const db = mysql.createConnection({
+    host: "localhost", 
+    user: "root",
+    password: "8!hUnrBeLKAeEftewsO#c%k^XJ@O0SMW$tm10p4AF^Gb4wS1G93JM6GfzJBlz^^CH!2NpWxvRBfno4GDuKJVKeCTdefp#^k**v4s8wqfGBDpf6yE8tf&HYlbFW!%&H56",
+    database: "bootcampday3"
+});
+
 // Route
 app.get("/", (req, res) => {
     res.send("Hello ITB")
