825df8233ef21150f5a094a7fa3be72db3f75ac7
KungKurma <cocosaboko@protonmail.ch>
2021-09-18 14:27:25 +0700

    main

--- Day3/index.js
+++ Day3/index.js
@@ -5,7 +5,16 @@ const mysql = require('mysql');
 // Panggil express function
 const app = express();
 
+// Route
+app.get("/", (req, res) => {
+    res.send("Hello ITB")
+});
+
+app.get("/test", (req, res) => {
+    res.send("Hello World")
+});
+
 // Start Server
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('Server started...')
 });
